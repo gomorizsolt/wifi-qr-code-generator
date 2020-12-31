f2442a7136bd9a163a917d6f59502c28468e9ddb
Zsolt Gömöri <zsgomoridev@icloud.com>
2020-12-31 14:26:13 +0100

    project init, install tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import qrcode from 'qrcode';
 
+// TODO remove empty spaces, sanitize input if needed
+// TODO escape special characters: / ; :
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [src, setSrc] = React.useState('');
+
+  // React.useEffect(() => {
+  //   async function getSrc() {
+  //     const src = await qrcode.toDataURL('test');
+
+  //     setSrc(src);
+  //   }
+
+  //   getSrc();
+  // }, []);
+
+  return <div className="App">{!!src && <img src={src} />}</div>;
 }
 
 export default App;
